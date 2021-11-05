b7f5212007e053d69119b4d52262008686a4df81
diegoxlz <diegoxlz@hotmail.com>
2021-11-05 19:53:27 -0300

    Subindo

---
+++ Flutter/pratica02/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+void main() {
+ runApp(
+ Center(
+ child: Text(
+ 'Olá, Mundo!! Meu nome é Diego',
+ textDirection: TextDirection.ltr,
+ ), //Text.
+ ), //Center.
+ );
+}
